bc2a95525766a01e27bf6aee0ec7562bf10c0547
Aaron J Drake <aaronjdrake@adapt-llc.com>
2021-07-31 15:31:47 -0400

    comment out

--- utils/wkhtmltopdf.js
+++ utils/wkhtmltopdf.js
@@ -6,28 +6,28 @@ module.exports = function (html, options = []) {
         console.log("1");
         const bufs = [];
         // const proc = spawn("/bin/sh", ["-o", "pipefail", "-c", `./bin/wkhtmltopdf ${options.join(" ")} - - | cat`]);
-        const proc = spawn("/bin/sh", ["ls"]);
+        // const proc = spawn("/bin/sh", ["ls"]);
 
-        proc.on("error", error => {
-            console.log("3");
-            reject(error);
-        }).on("exit", code => {
-            console.log("4");
-            if (code) {
-                reject(new Error(`wkhtmltopdf process exited with code ${code}`));
-            } else {
-                resolve(Buffer.concat(bufs));
-            }
-        });
-        console.log("5");
-        proc.stdin.end(html);
-        console.log("6");
-        proc.stdout.on("data", data => {
-            console.log("7");
-            bufs.push(data);
-        }).on("error", error => {
-            console.log("8");
-            reject(error);
-        });
+        // proc.on("error", error => {
+        //     console.log("3");
+        //     reject(error);
+        // }).on("exit", code => {
+        //     console.log("4");
+        //     if (code) {
+        //         reject(new Error(`wkhtmltopdf process exited with code ${code}`));
+        //     } else {
+        //         resolve(Buffer.concat(bufs));
+        //     }
+        // });
+        // console.log("5");
+        // proc.stdin.end(html);
+        // console.log("6");
+        // proc.stdout.on("data", data => {
+        //     console.log("7");
+        //     bufs.push(data);
+        // }).on("error", error => {
+        //     console.log("8");
+        //     reject(error);
+        // });
     }));
 };

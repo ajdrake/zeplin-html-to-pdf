a35f5d6c6379fed02fe0cec03f9349017ca98d55
Aaron J Drake <aaronjdrake@adapt-llc.com>
2021-07-31 14:13:10 -0400

    added console log

--- utils/wkhtmltopdf.js
+++ utils/wkhtmltopdf.js
@@ -2,23 +2,29 @@ const { spawn } = require("child_process");
 
 module.exports = function (html, options = []) {
     return new Promise(((resolve, reject) => {
+        console.log("1");
         const bufs = [];
         const proc = spawn("/bin/sh", ["-o", "pipefail", "-c", `wkhtmltopdf ${options.join(" ")} - - | cat`]);
+        console.log("2");
         proc.on("error", error => {
+            console.log("3");
             reject(error);
         }).on("exit", code => {
+            console.log("4");
             if (code) {
                 reject(new Error(`wkhtmltopdf process exited with code ${code}`));
             } else {
                 resolve(Buffer.concat(bufs));
             }
         });
-
+        console.log("5");
         proc.stdin.end(html);
-
+        console.log("6");
         proc.stdout.on("data", data => {
+            console.log("7");
             bufs.push(data);
         }).on("error", error => {
+            console.log("8");
             reject(error);
         });
     }));
